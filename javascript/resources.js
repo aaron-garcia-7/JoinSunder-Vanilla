310e36e3a81159e513e39cc76a18bf1d5eb92f87
Aaron Garcia <aaronegarcia1@gmail.com>
2023-03-16 08:17:02 -0600

    Added all pages

---
+++ javascript/resources.js
@@ -0,0 +1,59 @@
+//
+// Handling Navigation Logic
+//
+
+// Global elements
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav");
+const logo = document.querySelector(".logo");
+const navLogo = document.querySelector(".navLogo");
+
+// Page Unique elements
+const mainContent = document.querySelector(".mainContent");
+
+let navState = false;
+
+const toggleNav = () => {
+  navState = !navState;
+
+  if (navState) {
+    burger.classList.add("burgerActive");
+    nav.classList.add("navActive");
+    logo.classList.add("logoActive");
+    navLogo.classList.add("navLogoActive");
+    mainContent.classList.add("mainContentActive");
+  } else {
+    burger.classList.remove("burgerActive");
+    nav.classList.remove("navActive");
+    logo.classList.remove("logoActive");
+    navLogo.classList.remove("navLogoActive");
+    mainContent.classList.remove("mainContentActive");
+  }
+};
+
+const closeNav = () => {
+  navState = false;
+
+  if (navState) {
+    burger.classList.add("burgerActive");
+    nav.classList.add("navActive");
+    logo.classList.add("logoActive");
+    navLogo.classList.add("navLogoActive");
+    mainContent.classList.add("mainContentActive");
+  } else {
+    burger.classList.remove("burgerActive");
+    nav.classList.remove("navActive");
+    logo.classList.remove("logoActive");
+    navLogo.classList.remove("navLogoActive");
+    mainContent.classList.remove("mainContentActive");
+  }
+};
+
+burger.addEventListener("click", toggleNav);
+mainContent.addEventListener("click", closeNav);
+
+//
+// GSAP Animations
+//
+
+gsap.registerPlugin(ScrollTrigger);
