e070037c4343ea137a5227b22b12f07a00d28915
Aaron Garcia <aaronegarcia1@gmail.com>
2023-04-14 20:30:12 -0600

    Benchmark for compete page. Began tab-indexing

--- javascript/compete.js
+++ javascript/compete.js
@@ -4,24 +4,10 @@ const nav = document.querySelector(".nav");
 const logo = document.querySelector(".logo");
 const navLogo = document.querySelector(".navLogo");
 const activePage = document.querySelector("#activePage");
+const firstNavLink = document.querySelector("#firstNavLink");
 
 // Page Unique elements
 const mainContent = document.querySelector(".mainContent");
-
-//
-// CTA 2 Hover Animation
-//
-
-// const cta2 = document.querySelector(".cta2");
-// const cta2Circle = document.querySelector("#cta2Circle");
-
-// cta2.addEventListener("mousemove", (e) => {
-//   const rect = cta2.getBoundingClientRect(),
-//     x = e.clientX - rect.left,
-//     y = e.clientY - rect.top;
-//   cta2Circle.style.setProperty("--mouse-x", `${y}px`);
-//   cta2Circle.style.setProperty("--mouse-y", `${x}px`);
-// });
 
 //
 // Handling Navigation Logic
@@ -65,6 +51,24 @@ const closeNav = () => {
   }
 };
 
+const openNav = () => {
+  navState = true;
+
+  if (navState) {
+    burger.classList.add("burgerActive");
+    nav.classList.add("navActive");
+    logo.classList.add("logoActive");
+    navLogo.classList.add("navLogoActive");
+    mainContent.classList.add("mainContentActive");
+  } else {
+    burger.classList.remove("burgerActive");
+    nav.classList.remove("navActive");
+    logo.classList.remove("logoActive");
+    navLogo.classList.remove("navLogoActive");
+    mainContent.classList.remove("mainContentActive");
+  }
+};
+
 burger.addEventListener("click", toggleNav);
 mainContent.addEventListener("click", closeNav);
 activePage.addEventListener("click", closeNav);
@@ -75,9 +79,9 @@ activePage.addEventListener("click", closeNav);
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2, // use breakpoints below
-  // spaceBetween: 30,
-  // spaceBetween: "240px",
+  spaceBetween: 20,
   // loop: true,
+  // centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -87,7 +91,7 @@ var swiper = new Swiper(".mySwiper", {
   //   prevEl: ".swiper-button-prev",
   // },
   autoplay: {
-    delay: 4000,
+    delay: 6000,
     disableOnInteraction: false,
   },
   breakpoints: {
@@ -106,94 +110,99 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// //
-// // Handling Video Container Logic
-// //
+//
+// Handling Video Container Logic
+//
 
-// const videoContainer = document.querySelector(".videoContainer");
-// const closeVideoBurger = document.querySelector(".closeVideoBurger");
-// const videoElement = document.querySelector("#videoElement");
+const videoContainer = document.querySelector(".videoContainer");
+const closeVideoBurger = document.querySelector(".closeVideoBurger");
+const videoElement = document.querySelector("#videoElement");
 
-// let videoState = false;
+let videoState = false;
 
-// const toggleVideo = () => {
-//   videoState = !videoState;
-//   videoElement.play();
+const toggleVideo = () => {
+  videoState = !videoState;
+  videoElement.play();
 
-//   if (videoState) {
-//     videoContainer.classList.add("videoContainerActive");
-//     closeVideoBurger.classList.add("closeVideoBurgerActive");
-//   } else {
-//     videoContainer.classList.remove("videoContainerActive");
-//     closeVideoBurger.classList.remove("closeVideoBurgerActive");
-//   }
-// };
-// const openVideo = () => {
-//   videoState = true;
-//   videoElement.play();
+  if (videoState) {
+    videoContainer.classList.add("videoContainerActive");
+    closeVideoBurger.classList.add("closeVideoBurgerActive");
+  } else {
+    videoContainer.classList.remove("videoContainerActive");
+    closeVideoBurger.classList.remove("closeVideoBurgerActive");
+  }
+};
+const openVideo = () => {
+  videoState = true;
+  videoElement.play();
 
-//   if (videoState) {
-//     videoContainer.classList.add("videoContainerActive");
-//   } else {
-//     videoContainer.classList.remove("videoContainerActive");
-//   }
-// };
-// const closeVideo = () => {
-//   videoState = false;
-//   videoElement.pause();
+  if (videoState) {
+    videoContainer.classList.add("videoContainerActive");
+  } else {
+    videoContainer.classList.remove("videoContainerActive");
+  }
+};
+const closeVideo = () => {
+  videoState = false;
+  videoElement.pause();
 
-//   if (videoState) {
-//     videoContainer.classList.add("videoContainerActive");
-//   } else {
-//     videoContainer.classList.remove("videoContainerActive");
-//   }
-// };
+  if (videoState) {
+    videoContainer.classList.add("videoContainerActive");
+  } else {
+    videoContainer.classList.remove("videoContainerActive");
+  }
+};
 
-// cta2.addEventListener("click", () => {
-//   toggleVideo();
-// });
-// closeVideoBurger.addEventListener("click", () => {
-//   toggleVideo();
-//   videoElement.pause();
-// });
+closeVideoBurger.addEventListener("click", () => {
+  toggleVideo();
+  videoElement.pause();
+});
 
-// videoContainer.addEventListener("click", ({ target }) => {
-//   if (target.classList.contains("videoContainer")) {
-//     toggleVideo();
-//     videoElement.pause();
-//   }
-// });
-// videoElement.addEventListener("focus", () => {
-//   openVideo();
-// });
+videoContainer.addEventListener("click", ({ target }) => {
+  if (target.classList.contains("videoContainer")) {
+    toggleVideo();
+    videoElement.pause();
+  }
+});
+videoElement.addEventListener("focus", () => {
+  openVideo();
+});
 
-// const thumbnail1 = document.querySelector(".thumbnail1");
-// const thumbnail2 = document.querySelector(".thumbnail2");
-// const thumbnail3 = document.querySelector(".thumbnail3");
-// const thumbnail4 = document.querySelector(".thumbnail4");
-// const thumbnail5 = document.querySelector(".thumbnail5");
+let allPosters = document.querySelectorAll(".poster");
 
-// thumbnail1.addEventListener("click", () => {
-//   videoElement.src = "./assets/promo1.mp4";
-//   toggleVideo();
-//   // videoElement.play();
-// });
-// thumbnail2.addEventListener("click", () => {
-//   videoElement.src = "./assets/promo2.mp4";
-//   toggleVideo();
-// });
-// thumbnail3.addEventListener("click", () => {
-//   videoElement.src = "./assets/promo3.mp4";
-//   toggleVideo();
-// });
-// thumbnail4.addEventListener("click", () => {
-//   videoElement.src = "./assets/promo4.mp4";
-//   toggleVideo();
-// });
-// thumbnail5.addEventListener("click", () => {
-//   videoElement.src = "./assets/promo5.mp4";
-//   toggleVideo();
-// });
+allPosters.forEach((obj, index) => {
+  obj.addEventListener("click", () => {
+    // console.log(`clicked on poster ${index}`);
+    videoElement.src = `./assets/promo${index}.mp4`;
+    toggleVideo();
+  });
+  obj.addEventListener("keydown", (e) => {
+    // console.log(e);
+    if (e.code === "Enter") {
+      videoElement.src = `./assets/promo${index}.mp4`;
+      toggleVideo();
+    }
+  });
+  obj.addEventListener("focusin", () => {
+    closeVideo();
+    closeNav();
+  });
+});
+
+logo.addEventListener("focusin", () => {
+  closeVideo();
+  closeNav();
+});
+
+navLogo.addEventListener("focusin", () => {
+  openNav();
+  closeVideo();
+});
+
+firstNavLink.addEventListener("focusin", () => {
+  openNav();
+  closeVideo();
+});
 
 //
 // GSAP Animations
