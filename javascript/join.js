4e9af6fe70871034e5ed59c7a0d4f0131cb0aecc
Aaron Garcia <aaronegarcia1@gmail.com>
2023-04-22 21:07:30 -0600

    Progress to join page

--- javascript/join.js
+++ javascript/join.js
@@ -5,7 +5,7 @@
 // Global elements
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
-const logo = document.querySelector(".logo");
+// const logo = document.querySelector(".logo");
 const navLogo = document.querySelector(".navLogo");
 
 // Page Unique elements
@@ -19,13 +19,13 @@ const toggleNav = () => {
   if (navState) {
     burger.classList.add("burgerActive");
     nav.classList.add("navActive");
-    logo.classList.add("logoActive");
+    // logo.classList.add("logoActive");
     navLogo.classList.add("navLogoActive");
     mainContent.classList.add("mainContentActive");
   } else {
     burger.classList.remove("burgerActive");
     nav.classList.remove("navActive");
-    logo.classList.remove("logoActive");
+    // logo.classList.remove("logoActive");
     navLogo.classList.remove("navLogoActive");
     mainContent.classList.remove("mainContentActive");
   }
@@ -37,13 +37,13 @@ const closeNav = () => {
   if (navState) {
     burger.classList.add("burgerActive");
     nav.classList.add("navActive");
-    logo.classList.add("logoActive");
+    // logo.classList.add("logoActive");
     navLogo.classList.add("navLogoActive");
     mainContent.classList.add("mainContentActive");
   } else {
     burger.classList.remove("burgerActive");
     nav.classList.remove("navActive");
-    logo.classList.remove("logoActive");
+    // logo.classList.remove("logoActive");
     navLogo.classList.remove("navLogoActive");
     mainContent.classList.remove("mainContentActive");
   }
